ff82fd89a8d20b2077d8369e8d1392f5c4ee1588
petetetete <ph289@nau.edu>
2017-09-28 06:42:50 +0100

    Add Camera, Sphere, and Plane object structures

--- raycast.c
+++ raycast.c
@@ -2,5 +2,14 @@
 #include "raycast.h"
 
 int main(int argc, char *argv[]) {
+
+  Object *test = malloc(sizeof(Object));
+  Sphere *sphere = malloc(sizeof(Sphere));
+
+  test->kind = OBJECT_KIND_SPHERE;
+  sphere->kind = OBJECT_KIND_SPHERE;
+
+  printf("%d", sphere->kind);
+
   return 0;
 }
--- raycast.h
+++ raycast.h
@@ -1,10 +1,43 @@
 #ifndef RAYCAST_H
 #define RAYCAST_H
 
-// Error code constants
-#define NO_STRING_FOUND -1
-#define MALFORMED_HEADER -2
+// Include standard libraries
+#include <stdlib.h>
+#include <stdio.h>
 
+// Define constants
+#define OBJECT_KIND_CAMERA 1
+#define OBJECT_KIND_SPHERE 2
+#define OBJECT_KIND_PLANE 3
 
+// Define types to be used in c file
+typedef struct Object Object;
+typedef struct Camera Camera;
+typedef struct Sphere Sphere;
+typedef struct Plane Plane;
+
+struct Object {
+  int kind;
+};
+
+struct Camera {
+  struct Object;
+  double width;
+  double height;
+};
+
+struct Sphere {
+  struct Object;
+  double color[3];
+  double position[3];
+  double radius;
+};
+
+struct Plane {
+  struct Object;
+  double color[3];
+  double position[3];
+  double normal[3];
+};
 
 #endif  // RAYCAST_H
